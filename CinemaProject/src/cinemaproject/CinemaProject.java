4f9c26f62e37a4fa5cc80381bb1d1b9d568a70a8
Viktoriia_Mironova <Viktoriia_Mironova@EPUAKYIW2702.kyiv.epam.com>
2019-02-19 11:57:21 +0200

    First line

--- CinemaProject/src/cinemaproject/CinemaProject.java
+++ CinemaProject/src/cinemaproject/CinemaProject.java
@@ -15,7 +15,7 @@ public class CinemaProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world from newBranch");
     }
     
 }
